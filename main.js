6708f4f06eb7b7b6d154f6553e2db7404c7a9c96
Duckster Boo <ibrahimkhan1234_5@icloud.com>
2024-05-30 14:59:16 +0100

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { rotate } from 'three/examples/jsm/nodes/Nodes.js';
 
 //---------------------------------------MOUSE INTERACTION BACKEND-------------------------------------------------------//
 
@@ -35,51 +34,92 @@ function manageRaycasterIntersections(scene, camera) {
     }
 }
 
-//------------------------------------------HTML MANIPULATION----------------------------------------------------//
-
-//const canvas = document.querySelector('canvas');
-
-//const name = document.getElementById('name');
-//name.style.top = `50px`;
-//name.style.left = `100px`;
-
-//const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
-//addCSS("img {width: 400px; height: 400px; transform: translate(" + x2 + "px, " + y2 + "px);}")
-
-//----------------------------------------------3D BUSINESS------------------------------------------------//
-/*
+//-------------------
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
-
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const textureLoader = new THREE.TextureLoader();
+
+const geometry = new THREE.BoxGeometry( 2.5, 2.5, 2.5 );
+//const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const texture = [
+    new THREE.MeshBasicMaterial({
+        map: textureLoader.load("assets/underCar.jpg"),
+    }),
+    new THREE.MeshBasicMaterial({
+        map: textureLoader.load("assets/kartGoing.png"),
+    }),
+    new THREE.MeshBasicMaterial({
+        color : 0x000000
+    }),
+    new THREE.MeshBasicMaterial({
+        color : 0x000000
+    }),
+    new THREE.MeshBasicMaterial({
+        map: textureLoader.load("assets/meDawg.png"),
+    }),
+    new THREE.MeshBasicMaterial({
+        color : 0x000000
+    }),
+]
+
+const cube = new THREE.Mesh( geometry, texture );
+scene.add( cube );
+
 camera.position.z = 5;
-
-const light = new THREE.DirectionalLight(0x404040, 100);
-scene.add( light );
-
-//--------------------------------------------ANIMATE--------------------------------------------------//
 
 function animate() {
 	requestAnimationFrame( animate );
 
-    if (model) {
+    cube.position.y = 0.6;
 
-        model.rotation.y = mouse.x;
-        model.rotation.x = -mouse.y;
-        //model.position.x = mouse.x;
-        //model.position.y = -mouse.y - 0.5;
-        
-       //model.rotation.y = mouse.x;
+    if(mouse.y <= 0.6 && mouse.y >= -0.26)
+    {
+        cube.rotation.y = mouse.x;
+        cube.position.x = mouse.x;
     }
+    
+    if(cube.rotation.y != 0)
+    {
+        if(cube.rotation.y > 0.03)
+        {
+            cube.rotation.y -= 0.05;
+        }
+        else if(cube.rotation.y < -0.03)
+        {
+            cube.rotation.y += 0.05;
+        }
+    }
+    else if(cube.rotation.y = 0)
+    {
+        cube.rotation.y = 0;
+    }
+    
+    if(cube.position.x != 0)
+    {
+        if(cube.position.x > 0.03)
+        {
+            cube.position.x -= 0.05;
+        }
+        else if(cube.position.x < -0.03)
+        {
+            cube.position.x += 0.05;
+        }
+    }
+    else if(cube.position.x = 0)
+    {
+        cube.position.x = 0;
+    }
+
 	renderer.render( scene, camera );
 }
+
 animate();
-*/
 
 //---------------------------------------------MOUSE EVENTS-------------------------------------------------//
 
