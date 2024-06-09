7576858a787fae1265407350a7ec473c932fff15
Duckster Boo <ibrahimkhan1234_5@icloud.com>
2024-06-10 00:40:04 +0100

    Updates

--- about.js
+++ about.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 //---------------------------------------MOUSE INTERACTION BACKEND-------------------------------------------------------//
 
